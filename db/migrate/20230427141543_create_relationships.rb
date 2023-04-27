723813ce0d598d1ce901882982e5e5fc2fff23b6
kazu <nabhir3008@gmail.com>
2023-04-27 23:32:44 +0900

    mentioning_idとmentioned_idに外部キーとユニーク制約を付けて、migrateした

--- db/migrate/20230427141543_create_relationships.rb
+++ db/migrate/20230427141543_create_relationships.rb
@@ -1,10 +1,12 @@
 class CreateRelationships < ActiveRecord::Migration[7.0]
   def change
     create_table :relationships do |t|
-      t.references :mentioning, null: false, foreign_key: true
-      t.references :mentioned, null: false, foreign_key: true
+      t.references :mentioning, null: false, foreign_key: { to_table: :reports }
+      t.references :mentioned, null: false, foreign_key: { to_table: :reports }
 
       t.timestamps
+
+      t.index %i[mentioning_id mentioned_id], unique: true
     end
   end
 end
