db3af87381edae9e4bc993677bf96f9f0f9850f5
kazu <nabhir3008@gmail.com>
2023-04-28 23:47:02 +0900

    日報の新規、更新、削除時に本文に他の日報のURLがあれば、他の日報に言及リンクを表示するようにした

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,6 +22,8 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new(report_params)
 
     if @report.save
+      mentioned_ids = find_mentioned_ids(@report.content)
+      create_mentioning_reports(mentioned_ids)
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
@@ -30,6 +32,10 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
+      old_mentioned_ids = @report.mentioning_report_ids
+      new_mentioned_ids = find_mentioned_ids(@report.content)
+      destroy_mentioning_reports(old_mentioned_ids - new_mentioned_ids)
+      create_mentioning_reports(new_mentioned_ids - old_mentioned_ids)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
@@ -50,5 +56,17 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:title, :content)
+  end
+
+  def find_mentioned_ids(content)
+    content.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.map(&:to_i).uniq
+  end
+
+  def create_mentioning_reports(mentioned_ids)
+    mentioned_ids.each { |mentioned_id| @report.active_relationships.build(mentioned_id:).save unless mentioned_id == @report.id }
+  end
+
+  def destroy_mentioning_reports(mentioned_ids)
+    mentioned_ids.each { |mentioned_id| @report.active_relationships.find_by(mentioned_id:).destroy }
   end
 end
