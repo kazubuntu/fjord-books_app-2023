8599613825ec9123feb3bf9200eab046d863e99b
kazu <nabhir3008@gmail.com>
2023-04-28 16:07:34 +0900

    ReportとRelationshipモデルを関連付けた

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,12 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :mentioning_id, inverse_of: 'mentioning', dependent: :destroy
+  has_many :mentioning_reports, through: :active_relationships, source: :mentioned
+
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :mentioned_id, inverse_of: 'mentioned', dependent: :destroy
+  has_many :mentioned_reports, through: :passive_relationships, source: :mentioning
+
   validates :title, presence: true
   validates :content, presence: true
 
