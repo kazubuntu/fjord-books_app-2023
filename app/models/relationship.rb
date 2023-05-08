50bb3ff10639af62efbf94dbc7c7be2fc65a3787
kazu <nabhir3008@gmail.com>
2023-05-09 08:14:18 +0900

    言及元と言及先が一意である検証をモデルに追加した

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,6 @@
 class Relationship < ApplicationRecord
   belongs_to :mentioning, class_name: 'Report'
   belongs_to :mentioned, class_name: 'Report'
+
+  validates :mentioned_id, uniqueness: { scope: :mentioning_id }
 end
