d6ad097116c8ecedf077cd5c59b078ca7c761f93
kazu <nabhir3008@gmail.com>
2023-05-13 14:18:06 +0900

    ユーザーテストを作成した

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice = users(:alice)
+  end
+
+  test 'should return email' do
+    assert_equal 'alice@example.com', @alice.name_or_email
+  end
+
+  test 'should return name' do
+    @alice.name = 'alice'
+    assert_equal 'alice', @alice.name_or_email
+  end
 end
