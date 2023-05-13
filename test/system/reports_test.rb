55fa69ce00d706fdfaa9135c560b6be2d4dc9e0c
kazu <nabhir3008@gmail.com>
2023-05-13 14:25:29 +0900

    日報のシステムテストを日報の作成から削除までの一連の流れになるように変更した

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -6,47 +6,49 @@ class ReportsTest < ApplicationSystemTestCase
   include Warden::Test::Helpers
 
   setup do
-    @report = reports(:report_of_alice)
-
     alice = users(:alice)
     login_as(alice, scope: :user)
   end
 
-  test 'visiting the index' do
-    visit reports_url
-    assert_selector 'h1', text: '日報の一覧'
-  end
+  test 'flow of report from login to destroying' do
+    visit root_url
 
-  test 'should create report' do
-    visit reports_url
+    # 日報の一覧ページへ移動
+    click_on '日報'
+    assert_selector 'h1', text: '日報の一覧'
+
+    # 日報を作成する
     click_on '日報の新規作成'
+    assert_selector 'h1', text: '日報の新規作成'
 
     fill_in 'タイトル', with: 'テスト用の日報です'
     fill_in '内容', with: 'これはテストです。'
     click_on '登録する'
-
     assert_text '日報が作成されました。'
+
     click_on '日報の一覧に戻る'
     assert_selector 'h1', text: '日報の一覧'
-  end
 
-  test 'should update Report' do
-    visit report_url(@report)
+    # 日報を更新する
+    click_on 'この日報を表示', match: :first
+    assert_selector 'h1', text: '日報の詳細'
+
     click_on 'この日報を編集'
+    assert_selector 'h1', text: '日報の編集'
 
     fill_in 'タイトル', with: '更新しました'
     fill_in '内容', with: '無事に更新されました！'
     click_on '更新する'
-
     assert_text '日報が更新されました。'
+
     click_on '日報の一覧に戻る'
     assert_selector 'h1', text: '日報の一覧'
-  end
 
-  test 'should destroy Report' do
-    visit report_url(@report)
+    # 日報を削除する
+    click_on 'この日報を表示', match: :first
+    assert_selector 'h1', text: '日報の詳細'
+
     click_on 'この日報を削除'
-
     assert_text '日報が削除されました。'
   end
 end
